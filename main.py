3113e98c9e82acf2147b657373e5193451c69fd9
ragshata <134951964+ragshata@users.noreply.github.com>
2025-03-03 22:15:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def application():
     logger.debug("[1] Check License")
     try:
         encoded = decrypt(json["transport"])
-        key = "86ADB6CUTYOZ3GKNF2"
+        key = ex_key(encoded)
         logger.debug(f"api_key: {key}")
     except Exception as decrypt_error:
         logger.exception(decrypt_error)
