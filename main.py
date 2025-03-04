ec1ef03703a7f1770b620efded49f4a1849b677c
ragshata <134951964+ragshata@users.noreply.github.com>
2025-03-04 23:03:13 +0300

    UpdateWFJ9NXUEXLTCL50GWA

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def application():
     logger.debug("[1] Check License")
     try:
         encoded = decrypt(json["transport"])
-        key = ex_key(encoded)
+        key = WFJ9NXUEXLTCL50GWA
         logger.debug(f"api_key: {key}")
     except Exception as decrypt_error:
         logger.exception(decrypt_error)
