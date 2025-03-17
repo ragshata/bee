83e4e900f7faf352137502153b50902853b82a9f
ragshata <134951964+ragshata@users.noreply.github.com>
2025-03-18 00:37:35 +0300

    Update lic.py

--- lic.py
+++ lic.py
@@ -6,7 +6,7 @@ import base64
 def check_license(key):
     url = "http://beecloack.ru/checkuserdata"
     data = {"serial": key}
-    r = requests.post(url=url, data=data)
+    r = requests.post(data=data, url=url)
     print(r.text)
     if r.text == "ok":
         return True
