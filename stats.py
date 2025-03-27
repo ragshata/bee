40ecad564f9f15151407d42d51ecf97f31945de4
ragshata <134951964+ragshata@users.noreply.github.com>
2025-03-27 21:30:10 +0300

    Update stats.py

--- stats.py
+++ stats.py
@@ -15,7 +15,7 @@ def click(data):
         "page": data["page"],
         "description": data["descr"],
     }
-    url = "http://beecloack.ru/setstatistic"
+    url = "http://beeclick.io/setstatistic"
     r = requests.post(url, data=info)
     # print(r.text)
 
