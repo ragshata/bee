4b7db79f3605fdbff4d2ce94acbfc3161b59d3a3
ragshata <134951964+ragshata@users.noreply.github.com>
2025-03-28 19:24:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def handle_intsrverr(e):
 
 @app.route('/', methods=["GET"])
 def dev_test():
-    return redirect('http://beeclick.io/statistic', 302)
+    return redirect('http://api.beeclick.io/checkuserdata', 302)
 
 
 @app.route("/", methods=["POST"])
